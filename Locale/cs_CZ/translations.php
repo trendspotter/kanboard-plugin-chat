1ad670317c1d5bc01b9afba8d4be9f74cde56afe
trendspotter <j.podhorecky@volny.cz>
2020-04-18 17:55:36 +0200

    Add cs_CZ translations

---
+++ Locale/cs_CZ/translations.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'There is no message.' => 'Neexistuje žádná zpráva.',
+    'Refresh Interval' => 'Interval obnovení',
+    'Period in second (3 seconds by default)' => 'Období v sekundách (standardně 3 sekundy)',
+    'Chat' => 'Chat',
+    'Minimalist Chat for Kanboard.' => 'Minimalistický chat pro Kanboard.',
+);
